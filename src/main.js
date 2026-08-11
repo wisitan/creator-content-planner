6d5ae4f9429f9ab7fedf114ee85b4fa551793d94
Avery <wisit.an@gmail.com>
2026-08-11 10:32:50 +0700

    fix(version): update APP_VERSION string to v2.2.5 in main.js UI shell

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { store } from './store.js';
 import { showToast } from './components/toast.js';
 import { initGoogleDrive, backupToDrive, syncFromDrive, smartSyncWithDrive } from './google-drive.js';
 
-const APP_VERSION = 'v2.2.3';
+const APP_VERSION = 'v2.2.5';
 
 export function applyTheme(theme) {
   if (theme === 'dark') {
